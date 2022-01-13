6ad02c556206a8f50024287540b6e94cb853b2f0
Abshir559 <abshir.adandalab@gmail.com>
2022-01-13 04:56:48 +0000

    added to the database

---
+++ db/seeds/02_products.sql
@@ -0,0 +1,14 @@
+insert into product_categories(id, name, description, thumbnail) values (1, 'Cosmetic','Any cosmetic product you may need.','https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beauty-products-1603140461.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*');
+
+insert into product_categories(id, name, description, thumbnail) values (2, 'Snacks','Something nice to eat when you are not hungry.','https://m.economictimes.com/thumb/msid-77885315,width-1200,height-900,resizemode-4,imgsize-260096/pesico-products-website.jpg');
+
+insert into products  (id, name, price, description, image, quantity,
+  category_id) values (1, 'Ruffles All Dressed', 500, 'Bag of Ruffles Brand Chips All Dressed', 'https://www.londondrugs.com/on/demandware.static/-/Sites-londondrugs-master/default/dw2aad3118/products/L8230625/large/L8230625.JPG',20, 2);
+
+insert into products  (id, name, price, description, image, quantity,
+  category_id) values (2, 'Cookies', 400, 'Bag of Ruffles Brand Chips All Dressed', 'https://www.londondrugs.com/on/demandware.static/-/Sites-londondrugs-master/default/dw2aad3118/products/L8230625/large/L8230625.JPG',40, 2);
+
+  insert into products  (id, name, price, description, image, quantity,
+  category_id) values (3, 'Make UP', 700, 'Bag of Ruffles Brand Chips All Dressed', 'https://www.londondrugs.com/on/demandware.static/-/Sites-londondrugs-master/default/dw2aad3118/products/L8230625/large/L8230625.JPG',50, 1);
+
+
